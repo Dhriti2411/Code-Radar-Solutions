6e7660e8f44e3318a5905478b180a05ddd5c4f0b
Dhriti2411 <dhritibhagria@gmail.com>
2025-02-10 09:31:48 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,7 +6,7 @@ int main(){
     if (n=='a','e','i','o','u','A','E','I','O','U'){
         printf("Vowel");
     }
-    else if ((n>='A' && n<= 'Z') && (n!='a','e','i','o','u','A','E','I','O','U')){
+    else{
         printf("Consonant");
     }
     else if ((n>='a' && n<= 'z') && (n!='a','e','i','o','u','A','E','I','O','U')){
